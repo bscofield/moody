d1ecc08df92af8a952541385836c02cccdcc73ad
Ben Scofield <git@turrean.com>
2013-12-09 17:54:07 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moody::Application.config.secret_key_base = '16f813ac3f69cac47b2b3cb372986a9bec9a21e08e133844bbb518c40b15308a2ab9d8a9822d101863246a9fc0d69fe1facb95a5f61c5ecef063fefc4518b927'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moody::Application.config.session_store :cookie_store, key: '_moody_session'
