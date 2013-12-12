a5b67e97b882ee26f13e10ea9de3afc6c1b9ab8e
Ben Scofield <git@turrean.com>
2013-12-12 18:58:54 -0500

    circles

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -1,11 +1,10 @@
 var now = new Date();
 var width = $('#timeline').width();
-var bar_width = 15;
-var bar_height = bar_width * 2;
+var radius = 10;
 
 var y = d3.scale.linear()
           .domain([-1,1])
-          .range([bar_height*2-10, bar_height-10]);
+          .range([radius*4-10, radius*2-10]);
 var x = d3.scale.linear()
           .domain([data[0].recorded_at.getTime()-10000000, now.getTime()+10000000])
           .range([0, width]);
@@ -13,7 +12,7 @@ var x = d3.scale.linear()
 var timeline = d3.select('#timeline')
              .append('svg')
                  .attr('width', width)
-                 .attr('height', bar_height*3)
+                 .attr('height', radius*6)
                  .append('g');
 
 
@@ -27,33 +26,41 @@ timeline.selectAll("line")
         .attr("y2", y(0))
         .style("stroke", "lightgray");
 
-var bar = timeline.selectAll('g')
-                  .data(data)
-                  .enter()
-                  .append('g')
-                  .attr('transform', function(d, i) { return "translate(" + x(d.recorded_at.getTime()) + ",0)"; });
+                  // .attr('transform', function(d, i) { return "translate(" + x(d.recorded_at.getTime()) + ",0)"; });
+                  // attr("r", function(d) { return d / max * 14; }).
+                  // attr("transform", function() {
+                  //     tx = pane_left - 2 * margin + x(j);
+                  //     ty = height - 7 * margin - y(i);
+                  //     return "translate(" + tx + ", " + ty + ")";
+                  //   });
 
-bar.append('rect')
-   .attr('y', function(d) { return y(d.score) - bar_height/2; })
-   .attr('height', bar_height)
-   .attr('width', bar_height/2)
-   .style('fill', function(d) {
-     switch(d.score) {
-       case 1:
-         return "rgba(51,153,255,0.5)"
-       case -1:
-         return "rgba(153,0,0,0.5)"
-       default:
-         return "rgba(204,204,204,0.5)"
-     }
-   })
-   .on("mouseover", mover)
-   .on("mouseout", mout)
-   .on("mousemove", function() {
-    return tooltip.
-      style("top", (d3.event.pageY - 10) + "px").
-      style("left", (d3.event.pageX + 10) + "px");
-   });
+timeline.append('g')
+        .selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('r', radius)
+        .attr('transform', function(d, i) {
+          tx = x(d.recorded_at.getTime()) - radius;
+          ty = y(d.score);
+          return "translate(" + tx + ", " + ty + ")";
+        })
+        .style('fill', function(d) {
+          switch(d.score) {
+            case 1:
+              return "rgba(51,153,255,0.5)"
+            case -1:
+              return "rgba(153,0,0,0.5)"
+            default:
+              return "rgba(153,153,153,0.5)"
+          }
+        })
+        .on("mouseover", mover)
+        .on("mouseout", mout)
+        .on("mousemove", function() {
+          return tooltip.style("top", (d3.event.pageY - 10) + "px").
+                         style("left", (d3.event.pageX + 10) + "px");
+        });
 
 function mover(d) {
   tooltip = d3.select("body")
